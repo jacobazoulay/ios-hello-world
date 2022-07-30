71d8a852cd795cdc4e373294a91c19bd0caec8c0
Jacob Azoulay <jacobazoulay@gmail.com>
2022-07-30 11:50:06 -0700

    initial commit

---
+++ helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloworld
+//
+//  Created by Jacob Azoulay on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloworld/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  helloworld
+//
+//  Created by Jacob Azoulay on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
